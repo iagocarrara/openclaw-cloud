4420b2c9485416c6f878ad2e8dd91bcab6393acd
iagocarrara <iagocarrara@gmail.com>
2026-02-25 19:43:22 -0300

    fix port to 8080

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import http from "http";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
