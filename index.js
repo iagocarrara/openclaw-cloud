dde786d80a6cd569b0e4d79c31186e09441eefc6
iagocarrara <iagocarrara@gmail.com>
2026-02-25 19:58:14 -0300

    fix utf8 encoding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import http from "http";
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
   res.end("OpenClaw está online 🚀");
 });
 
